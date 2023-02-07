3858173c191e2d75693295a6cc366f8962809029
juanca2918 <jacal2211@gmail.com>
2023-02-06 21:14:40 -0500

    Se modifican index json y se eliminan log

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
+//Dependencias
 const { google } = require('googleapis')
 const twilio = require('twilio');
 const moment = require('moment');
-const { createLogger, format, transports } = require('winston');
+const express = require('express');
+const app = express();
 require('dotenv').config()
 
-const logger = createLogger({
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
-  transports: [
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' }),
-    new transports.Console({
-      format: format.combine(
-        format.colorize(),
-        format.simple()
-      )
-    })
-  ]
-});
-
-
+//Variables de entorno
 const clientIdent = process.env.CLIENTID
 const clientSecrets = process.env.CLIENTSECRET
 const UriAuth = process.env.AUTHURI
@@ -36,31 +17,32 @@ const spreadsheetId = process.env.YOUR_SPREADSHEET_ID
 const twilioPhoneNum = process.env.YOUR_TWILIO_PHONE_NUMBER
 
 //Funcion principal
-async function sendSmsFromSheet() {
-  // Authenticate using the service account key
+async function sendSmsFromSheet(req, res) {
+  // Autenticate usando GoogleAPIS OAuth2
   const Authenticate = new google.auth.OAuth2({
     client_id: clientIdent,
     client_secret: clientSecrets,
     auth_uri: UriAuth,
     token_uri: tokenUri
-   });
-  logger.info(Authenticate)
+  });
+  
+  // ... Usa el objeto traido de googleapis con los datos de la constante Authenticate para logear en google
   const sheets = google.sheets({version: 'v4', auth:Authenticate});
-    // ... Use the sheets object to access the Google Sheets API
-  logger.info(sheets)
-  // Read data from the Google Sheet
+
+  // Lee datos de google sheet
   const sheet = await sheets.spreadsheets.values.get({
     spreadsheetId: `${spreadsheetId}`,
     range: 'Sheet1!A1:E1',
   });
   const rows = sheet.data.values;
 
-  // Initialize the Twilio client
+  // Inicializa el cliente de twilio
   const client = twilio(
     `${twilioAccount}`,
     `${twilioAuth}`
   );
 
+  //Para saber que momento del dia es y asi saludar a los usuarios depende de la hora del dia
   let hour = moment().format('HH');
   let saludo = ''
   if (hour >= 6 && hour < 12) {
@@ -71,7 +53,7 @@ async function sendSmsFromSheet() {
     saludo = 'Buenas noches!';
   }
 
-  // Loop over the rows in the sheet
+  // Loop sobre la fila o row que trae el objeto sheet con toda su informacion
   for (const row of rows) {
     const [, , date, name, phoneNumber] = row;
     const appointmentDatetime = moment(date, 'MM/DD/YYYY HH:mm');
@@ -81,8 +63,9 @@ async function sendSmsFromSheet() {
     let messageOneDay = `Buenas ${saludo}, Señor(a) ${name}, le recordamos que tiene cita en la fecha ${appointmentDatetime}`
     let messagethirtymin = `Buenas ${saludo}, Señor(a) ${name}, le recordamos que tiene cita en 30 min`
     logger.info(`Starting the SMS sending job at ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
-    // Check if the current date is equal to or after one day before the appointment
-    if (currentDate >= oneDayBefore) {
+
+    // Check Si la fecha actual es estrictamente igual que un dia antes
+    if (currentDate === oneDayBefore) {
       // Send an SMS message with Twilio
       await client.messages.create({
         to: phoneNumber,
@@ -92,8 +75,8 @@ async function sendSmsFromSheet() {
       logger.info(`Sending SMS to ${phoneNumber} with message "${messageOneDay}"`);
     }
 
-    // Check if the current date is equal to or after 30 minutes before the appointment
-    if (currentDate >= thirtyMinutesBefore) {
+    // Check Si la fecha actual es estrictamente igual a 30 minutos antes
+    if (currentDate === thirtyMinutesBefore) {
       // Send an SMS message with Twilio
       await client.messages.create({
         to: phoneNumber,
@@ -103,13 +86,16 @@ async function sendSmsFromSheet() {
       logger.info(`Sending SMS to ${phoneNumber} with message "${messagethirtymin}"`);
     }
   }
+
+  res.send('Mensajes SMS enviados');
+
 }
 
-// Schedule the SMS sending job using cron
+// Programa el envio de los mensajes de texto por cron
 exports.handler = async function (event, context) {
   const job = new cron.CronJob('0 0 * * *', sendSmsFromSheet, null, true, 'America/Bogota');
   job.start();
   return { statusCode: 200, body: 'SMS sending job started' }
 }
 
-logger.info(`SMS sending job finished`);
+app.get('/', sendSmsFromSheet);
