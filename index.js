3291c08859127d327a5f0042ce65062897dc0c81
juanca2918 <jacal2211@gmail.com>
2023-02-08 15:14:32 -0500

    Se modifica index totalmente y se cambian valores de .env.example

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 //Dependencias
-const { google } = require('googleapis')
-const twilio = require('twilio');
 const moment = require('moment');
 const express = require('express');
 const app = express();
@@ -8,88 +6,70 @@ const path = require('path');
 require('dotenv').config()
 
 //Variables de entorno
-const clientIdent = process.env.CLIENTID
-const clientSecrets = process.env.CLIENTSECRET
-const UriAuth = process.env.AUTHURI
-const tokenUri = process.env.TOKENURI
 const twilioAccount = process.env.YOUR_TWILIO_ACCOUNT_SID
 const twilioAuth = process.env.YOUR_TWILIO_AUTH_TOKEN
 const spreadsheetId = process.env.YOUR_SPREADSHEET_ID
 const twilioPhoneNum = process.env.YOUR_TWILIO_PHONE_NUMBER
+const range = process.env.range
+const apiKey = process.env.APIKEY
 
 //Funcion principal
 async function sendSmsFromSheet(req, res) {
-  // Autenticate usando GoogleAPIS OAuth2
-  const Authenticate = new google.auth.OAuth2({
-    client_id: clientIdent,
-    client_secret: clientSecrets,
-    auth_uri: UriAuth,
-    token_uri: tokenUri
-  });
+  //Usamos fetch para solicitar a google sheet api la hoja de calculo desde donde obtendremos los datos
+  fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const fechacita = data.values[0][2];
+      const name = data.values[0][3];
+      const celular = data.values[0][4];
+      const scheduledTime = moment(fechacita, 'MM/DD/YYYY HH:mm')
+      //Para saber que momento del dia es y asi saludar a los usuarios depende de la hora del dia
+      let hour = moment().format('HH');
+      let saludo = ''
+      if (hour >= 6 && hour < 12) {
+        saludo = 'Buenos días!';
+      } else if (hour >= 12 && hour < 19) {
+        saludo = 'Buenas tardes!';
+      } else {
+        saludo = 'Buenas noches!';
+      }
+      let messageOneDay = `Buenas ${saludo}, Señor(a) ${name}, le recordamos que tiene cita en la fecha ${appointmentDatetime}`
+      let messagethirtymin = `Buenas ${saludo}, Señor(a) ${name}, le recordamos que tiene cita en 30 min`
 
-  // ... Usa el objeto traido de googleapis con los datos de la constante Authenticate para logear en google
-  const sheets = google.sheets({ version: 'v4', auth: Authenticate });
-
-  // Lee datos de google sheet
-  const sheet = await sheets.spreadsheets.values.get({
-    spreadsheetId: `${spreadsheetId}`,
-    range: 'Sheet1!A1:E1',
-  });
-  const rows = sheet.data.values;
-
-  // Inicializa el cliente de twilio
-  const client = twilio(
-    `${twilioAccount}`,
-    `${twilioAuth}`
-  );
-
-  //Para saber que momento del dia es y asi saludar a los usuarios depende de la hora del dia
-  let hour = moment().format('HH');
-  let saludo = ''
-  if (hour >= 6 && hour < 12) {
-    saludo = 'Buenos días!';
-  } else if (hour >= 12 && hour < 19) {
-    saludo = 'Buenas tardes!';
-  } else {
-    saludo = 'Buenas noches!';
-  }
-
-  // Loop sobre la fila o row que trae el objeto sheet con toda su informacion
-  for (const row of rows) {
-    const [, , date, name, phoneNumber] = row;
-    const appointmentDatetime = moment(date, 'MM/DD/YYYY HH:mm');
-    const oneDayBefore = moment(appointmentDatetime).subtract(1, 'day');
-    const thirtyMinutesBefore = moment(appointmentDatetime).subtract(30, 'minutes');
-    const currentDate = moment();
-    let messageOneDay = `Buenas ${saludo}, Señor(a) ${name}, le recordamos que tiene cita en la fecha ${appointmentDatetime}`
-    let messagethirtymin = `Buenas ${saludo}, Señor(a) ${name}, le recordamos que tiene cita en 30 min`
-    logger.info(`Starting the SMS sending job at ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
-
-    // Check Si la fecha actual es estrictamente igual que un dia antes
-    if (currentDate === oneDayBefore) {
-      // Send an SMS message with Twilio
-      await client.messages.create({
-        to: phoneNumber,
-        from: twilioPhoneNum,
-        body: messageOneDay,
-      });
-      logger.info(`Sending SMS to ${phoneNumber} with message "${messageOneDay}"`);
-    }
-
-    // Check Si la fecha actual es estrictamente igual a 30 minutos antes
-    if (currentDate === thirtyMinutesBefore) {
-      // Send an SMS message with Twilio
-      await client.messages.create({
-        to: phoneNumber,
-        from: twilioPhoneNum,
-        body: messagethirtymin,
-      });
-      logger.info(`Sending SMS to ${phoneNumber} with message "${messagethirtymin}"`);
-    }
-  }
+      if (now.isSameOrBefore(scheduledTime.subtract(1, 'hour'))) {
+        // Envía el mensaje de texto con Twilio
+        return fetch(`https://api.twilio.com/2010-04-01/Accounts/${twilioAccount}/Messages.json`, {
+          method: 'POST',
+          headers: {
+            'Authorization': 'Basic ' + buf.toString(twilioAccount + ':' + twilioAuth),
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: `To=${celular}&From=${twilioPhoneNum}&Body=${messageOneDay}`
+        });
+      }
+      if (now.isSameOrBefore(scheduledTime.subtract(30, 'minutes'))) {
+        // Envía el mensaje de texto con Twilio
+        return fetch(`https://api.twilio.com/2010-04-01/Accounts/${twilioAccount}/Messages.json`, {
+          method: 'POST',
+          headers: {
+            'Authorization': 'Basic ' + buf.toString(twilioAccount + ':' + twilioAuth),
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: `To=${celular}&From=${twilioPhoneNum}&Body=${messagethirtymin}`
+        });
+      }
+    })
+    .then(response => {
+      if (response) {
+        console.log(response.status);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
   res.send('Mensajes SMS enviados');
-  
+
 }
 
 // Programa el envio de los mensajes de texto por cron
@@ -103,10 +83,10 @@ exports.handler = async function (event, context) {
 app.get('/', (req, res) => {
   // Crea la ruta absoluta al archivo HTML
   const filePath = path.join(__dirname, 'politicasprivacidad.html');
-  
+
   // Establece el tipo de contenido de la respuesta como text/html
   res.set('Content-Type', 'text/html');
-  
+
   // Envía el archivo HTML como respuesta a la petición
   res.sendFile(filePath);
 });
